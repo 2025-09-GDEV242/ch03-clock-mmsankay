0df232c386c330a910a95275e474417c5739cb0e
msankay <162857863+mmsankay@users.noreply.github.com>
2025-09-22 21:09:14 -0400

    Update ClockDisplay.java
    Update 24hoursinternal

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -26,7 +26,7 @@ public class ClockDisplay
      */
     public ClockDisplay()
     {
-        hours = new NumberDisplay(24);
+        hours = new NumberDisplay(12);
         minutes = new NumberDisplay(60);
         updateDisplay();
     }
@@ -38,7 +38,7 @@ public class ClockDisplay
      */
     public ClockDisplay(int hour, int minute)
     {
-        hours = new NumberDisplay(24);
+        hours = new NumberDisplay(12);
         minutes = new NumberDisplay(60);
         setTime(hour, minute);
     }
