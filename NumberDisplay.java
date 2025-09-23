fc7202c6e80728dc2ce6267510fb1469277f449a
msankay <162857863+mmsankay@users.noreply.github.com>
2025-09-22 21:13:18 -0400

    Update NumberDisplay.java
    Add NumberDisplay

--- NumberDisplay.java
+++ NumberDisplay.java
@@ -7,8 +7,8 @@
  * resulting in display values from 0 to 59. When incremented, the display 
  * automatically rolls over to zero when reaching the limit.
  * 
- * @author Michael Kölling and David J. Barnes
- * @version 2016.02.29
+ * @author Marian M Sankay
+ * @version 2025.22.25
  */
 public class NumberDisplay
 {
